32c89c68035772ce76beb7598b094774c1da7573
Tuan Vu <vuanhtuan112ll@gmail.com>
2025-08-07 22:44:18 +0700

    init commit

--- BT1/src/Student.java
+++ BT1/src/Student.java
@@ -1,37 +1,40 @@
 public class Student {
-      private String name,age, address;
-      private double diemToan,diemVan,diemAnh;
-      private int ID;
-      Student(String name,String age,String address,double diemToan,double diemVan,double diemAnh,int ID){
-          this.name=name;
-          this.age=age;
-          this.address=address;
-          this.diemToan=diemToan;
-          this.diemVan=diemVan;
-          this.diemAnh=diemAnh;
-          this.ID=ID;
-      }
-      Student(){
+    private String name, age, address;
+    private double diemToan, diemVan, diemAnh;
+    private int ID;
 
-      public String getName(){
-          return name;
-      }
-    public String getAddress() {
-        return address;
+    Student(String name, String age, String address, double diemToan, double diemVan, double diemAnh, int ID) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.diemToan = diemToan;
+        this.diemVan = diemVan;
+        this.diemAnh = diemAnh;
+        this.ID = ID;
     }
-    public String getAge() {
-        return age;
-    }
-    public double getDiemAnh() {
-        return diemAnh;
-    }
-    public double getDiemToan() {
-        return diemToan;
-    }
-    public double getDiemVan() {
-        return diemVan;
-    }
-    public int getID() {
-        return ID;
-    }
+
+    Student() {}
+
+        public String getName () {
+            return name;
+        }
+        public String getAddress () {
+            return address;
+        }
+        public String getAge () {
+            return age;
+        }
+        public double getDiemAnh () {
+            return diemAnh;
+        }
+        public double getDiemToan () {
+            return diemToan;
+        }
+        public double getDiemVan () {
+            return diemVan;
+        }
+        public int getID () {
+            return ID;
+        }
+
 }
